85752da5d1e086e54f3e7016237d3ddbafe5145a
garry474 <xgross12@vutbr.cz>
2022-02-28 22:22:08 +0100

    graph1 and update project

--- labs/03-vivado/multiplexer/multiplexer.srcs/sim_1/new/tb_mux_2bit_4to1.vhd
+++ labs/03-vivado/multiplexer/multiplexer.srcs/sim_1/new/tb_mux_2bit_4to1.vhd
@@ -1,50 +1,38 @@
-------------------------------------------------------------
---
--- Testbench for 4-bit binary comparator.
--- EDA Playground
---
--- Copyright (c) 2020-Present Tomas Fryza
--- Dept. of Radio Electronics, Brno Univ. of Technology, Czechia
--- This work is licensed under the terms of the MIT license.
---
-------------------------------------------------------------
 
 library ieee;
-use ieee.std_logic_1164.all;
+use ieee.std_logic_1164.ALL;
 
 ------------------------------------------------------------
 -- Entity declaration for testbench
 ------------------------------------------------------------
-entity tb_mux_2bit_4to1 is
+entity tb_mux_3bit_4to1 is
     -- Entity of testbench is always empty
-end entity tb_mux_2bit_4to1;
+end entity tb_mux_3bit_4to1;
 
 ------------------------------------------------------------
 -- Architecture body for testbench
 ------------------------------------------------------------
-architecture testbench of tb_mux_2bit_4to1 is
+architecture testbench of tb_mux_3bit_4to1 is
 
     -- Local signals
     signal s_a           : std_logic_vector(3 - 1 downto 0);
     signal s_b           : std_logic_vector(3 - 1 downto 0);
     signal s_c           : std_logic_vector(3 - 1 downto 0);
     signal s_d           : std_logic_vector(3 - 1 downto 0);
-    signal s_s           : std_logic_vector(2 - 1 downto 0);
-    signal s_f           : std_logic_vector(3 - 1 downto 0);
-
+    signal s_sel       	 : std_logic_vector(2 - 1 downto 0);
+    signal s_f         	 : std_logic_vector(3 - 1 downto 0);
 
 begin
-    -- Connecting testbench signals with mux_2bit_4to1
+    -- Connecting testbench signals with mux_3bit_4to1
     -- entity (Unit Under Test)
-    uut_mux_2bit_4to1 : entity work.mux_2bit_4to1
+    uut_mux_3bit_4to1 : entity work.mux_3bit_4to1
         port map(
-            a_i           => s_a,
-            b_i           => s_b,
-            c_i           => s_c,
-            d_i           => s_d,
-            s_i           => s_s,
-            f_o => s_f
-
+            a_i      => s_a,
+            b_i      => s_b,
+            c_i      => s_c,
+            d_i      => s_d,
+            sel_i    => s_sel,
+            f_o      => s_f
         );
 
     --------------------------------------------------------
@@ -52,24 +40,35 @@ begin
     --------------------------------------------------------
     p_stimulus : process
     begin
-        -- Report a note at the beginning of stimulus process
-        report "Stimulus process started" severity note;
-
-        -- First test case
-                s_b <= "0110"; -- 230067 (6)
-        		s_a <= "0111";  -- 230067 (7)
-        		wait for 100 ns;
-        -- Expected output
-        assert ((s_B_greater_A = '1') and
-                (s_B_equals_A  = '0') and
-                (s_B_less_A    = '0'))
-        -- If false, then report an error
-        report "Input combination 0110, 0111 FAILED" severity error;
-
-        -- WRITE OTHER TEST CASES HERE
-
-        -- Report a note at the end of stimulus process
-        report "Stimulus process finished" severity note;
+       -- Set one test case and wait 100 ns
+       	s_d		<= "000";
+        s_c 	<= "101"; 
+        s_b 	<= "110"; 
+        s_a 	<= "111";
+        s_sel 	<= "00";
+        wait for 100 ns;
+        
+        s_d		<= "010";
+        s_c 	<= "111"; 
+        s_b 	<= "010"; 
+        s_a 	<= "101";
+        s_sel 	<= "01";
+        wait for 100 ns;
+        
+        s_d		<= "111";
+        s_c 	<= "001"; 
+        s_b 	<= "010"; 
+        s_a 	<= "000";
+        s_sel	<= "10";
+        wait for 100 ns;
+        
+        s_d		<= "011";
+        s_c 	<= "101"; 
+        s_b 	<= "111"; 
+        s_a 	<= "000";
+        s_sel	<= "11";
+        wait for 100 ns;
         wait;
     end process p_stimulus;
+
 end architecture testbench;
--- labs/03-vivado/multiplexer/multiplexer.srcs/sources_1/new/mux_2bit_4to1.vhd
+++ labs/03-vivado/multiplexer/multiplexer.srcs/sources_1/new/mux_2bit_4to1.vhd
@@ -1,38 +1,28 @@
-------------------------------------------------------------
---
--- Example of 4-bit binary comparator using the when/else
--- assignments.
--- EDA Playground
---
--- Copyright (c) 2020-Present Tomas Fryza
--- Dept. of Radio Electronics, Brno Univ. of Technology, Czechia
--- This work is licensed under the terms of the MIT license.
---
-------------------------------------------------------------
 
 library ieee;
-use ieee.std_logic_1164.all;
+use ieee.std_logic_1164.ALL;
 
 ------------------------------------------------------------
--- Entity declaration for 4-bit binary comparator
+-- Entity declaration for three-bit wide 4-to-1 multiplexer
 ------------------------------------------------------------
-entity mux_2bit_4to1 is
-    port(
-    
-          a_i				: in  std_logic_vector(3 - 1 downto 0);
-          b_i				: in  std_logic_vector(3 - 1 downto 0);    
-          c_i				: in  std_logic_vector(3 - 1 downto 0);
-          d_i				: in  std_logic_vector(3 - 1 downto 0); 
-          s_i				: in  std_logic_vector(2 - 1 downto 0);         
-          f_o		        : out std_logic_vector(3 - 1 downto 0)
+entity mux_3bit_4to1 is
+    port (
+        a_i     : in  std_logic_vector(3 - 1 downto 0);
+        b_i     : in  std_logic_vector(3 - 1 downto 0);
+        c_i     : in  std_logic_vector(3 - 1 downto 0);           
+        d_i     : in  std_logic_vector(3 - 1 downto 0);
+        sel_i   : in  std_logic_vector(2 - 1 downto 0);
+        f_o     : out std_logic_vector(3 - 1 downto 0)
     );
-end entity mux_2bit_4to1;
+end entity mux_3bit_4to1;
 
 ------------------------------------------------------------
--- Architecture body for 4-bit binary comparator
+-- Architecture body for three-bit wide 4-to-1 multiplexer
 ------------------------------------------------------------
-architecture Behavioral of mux_2bit_4to1 is
+architecture Behavioral of mux_3bit_4to1 is
 begin
-	f_o <= '001' when (b_i > a_i) else '000';
-
+    f_o <= a_i when (sel_i = "00") else
+           b_i when (sel_i = "01") else
+           c_i when (sel_i = "10") else
+           d_i;
 end architecture Behavioral;
