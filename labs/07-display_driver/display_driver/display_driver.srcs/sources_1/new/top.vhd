62e850b10399139e2cf734462b03f11b054653d0
garry474 <xgross12@vutbr.cz>
2022-03-24 12:47:10 +0100

    upload files

---
+++ labs/07-display_driver/display_driver/display_driver.srcs/sources_1/new/top.vhd
@@ -0,0 +1,71 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 24.03.2022 12:33:54
+-- Design Name: 
+-- Module Name: top - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity top is
+    Port ( CLK100MHZ : in STD_LOGIC;
+           SW : in STD_LOGIC;
+           CA : out STD_LOGIC;
+           CB : out STD_LOGIC;
+           CC : out STD_LOGIC;
+           CD : out STD_LOGIC;
+           CE : out STD_LOGIC;
+           CF : out STD_LOGIC;
+           CG : out STD_LOGIC;
+           DP : out STD_LOGIC;
+           AN : out STD_LOGIC;
+           BTNC : in STD_LOGIC);
+end top;
+
+architecture Behavioral of top is
+  -- No internal signals
+begin
+
+  --------------------------------------------------------
+  -- Instance (copy) of driver_7seg_4digits entity
+  driver_seg_4 : entity work.driver_7seg_4digits
+      port map(
+          clk        => CLK100MHZ,
+          reset      => BTNC,
+          data0_i(3) => SW(3),
+          data0_i(2) => SW(2),
+          data0_i(1) => SW(1),
+          data0_i(0) => SW(0),
+          -- WRITE YOUR CODE HERE
+          dp_i => "0111",
+          -- WRITE YOUR CODE HERE
+      );
+
+  -- Disconnect the top four digits of the 7-segment display
+  AN(7 downto 4) <= b"1111";
+
+end architecture Behavioral;
